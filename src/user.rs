7aeb4ed2aad4f0ae4a73c1170e2ffce04a57721d
gellel <lindsay.gelle@gmail.com>
2019-12-19 22:42:23 +1100

    Adding traits to pub struct User

--- src/user.rs
+++ src/user.rs
@@ -1,9 +1,16 @@
 use chrono::{DateTime, Utc};
 use std::collections::{HashSet};
 
+#[derive(Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 pub struct User {
     pub created_at: DateTime<Utc>,
     pub id: String,
     pub name: mut String,
-    pub promises: Option<mut HashSet<String>>,
+}
+
+impl ToString for User {
+
+    fn to_string(&self) -> String {
+        serde_json::to_string(&self).unwrap()
+    }
 }
