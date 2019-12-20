5cf4a685c1bcbbc07d9dc1a5b9863060621870f4
gellel <lindsay.gelle@gmail.com>
2019-12-20 18:01:06 +1100

    Added properties to pub struct Promise + private trait *

--- src/promise.rs
+++ src/promise.rs
@@ -2,16 +2,39 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use std::collections::{HashSet};
 
+trait Break {
+    fn break_promise(&mut self);
+}
+
+trait Complete {
+    fn complete_promise(&mut self);
+}
+
+trait End {
+    fn end_promise(&mut self);
+}
+
+trait Kept {
+    fn kept_promise(&mut self);
+}
+
+trait Start {
+    fn start_promise(&mut self);
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Promise {
     pub actions: Option<HashSet<String>>,
+    pub broken: bool,
     pub category: String,
     pub complete: bool,
+    pub completed_at: Option<DateTime<Utc>>,
     pub contracts: Option<HashSet<String>>,
     pub created_at: DateTime<Utc>,
     pub description: String,
     pub end_at: Option<DateTime<Utc>>,
     pub id: String,
+    pub kept: bool,
     pub labels: Option<HashSet<String>>,
     pub name: String,
     pub start_at: Option<DateTime<Utc>>,
@@ -20,6 +43,17 @@ pub struct Promise {
     pub user_id: String,
 }
 
+impl Break for Promise {
+    fn break_promise(&mut self) {
+        self.broken = true;
+    }
+}
+
+impl Complete for Promise {
+    fn complete_promise(&mut self) {
+        self.completed_at = Some(Utc::now());
+    }
+}
 
 impl PartialEq for Promise {
     fn eq(&self, other: &Self) -> bool {
