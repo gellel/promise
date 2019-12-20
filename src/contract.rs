24a1c4c730179f6589195215826158191d39c867
gellel <lindsay.gelle@gmail.com>
2019-12-20 21:20:24 +1100

    Adding traits to pub struct Contract{}

--- src/contract.rs
+++ src/contract.rs
@@ -1,8 +1,10 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use std::cmp::{Ordering};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Contract {
+    pub contracted_to: String,
     pub created_at: DateTime<Utc>,
     pub id: String,
     pub signed: bool,
@@ -11,8 +13,29 @@ pub struct Contract {
     pub user_id: String,
 }
 
-impl ToString for Contract {
+impl Contract {}
 
+impl Eq for Contract {}
+
+impl Ord for Contract {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.created_at.cmp(&other.created_at)
+    }
+}
+
+impl PartialEq for Contract {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
+impl PartialOrd for Contract {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl ToString for Contract {
     fn to_string(&self) -> String {
         serde_json::to_string(&self).unwrap()
     }
