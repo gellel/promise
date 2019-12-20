5c086fb634cc8a0af57999ffaaaea12ffdbde67f
gellel <lindsay.gelle@gmail.com>
2019-12-20 21:22:14 +1100

    Added contract field to pub struct Action{}

--- src/action.rs
+++ src/action.rs
@@ -3,36 +3,27 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Action {
-    pub category: String,
+    pub category: Option<HashSet<String>>,
     pub created_at: DateTime<Utc>,
     pub completed: bool,
     pub completed_at: Option<DateTime<Utc>>,
-    pub description: String,
+    pub contracts: Option<HashSet<String>>,
+    pub description: Option<String>,
     pub id: String,
     pub labels: Option<mut HashSet<String>>,
-    pub name: String,
-    pub subcategory: String,
+    pub name: Option<String>,
+    pub subcategory: Option<string>,
     pub tasks: Option<mut HashSet<String>>,
     pub user_id: String,
 }
 
-impl Action {
-
-    pub fn complete_action(&mut self) {
-        self.completed = true;
-        self.completed_at = Utc::now();
-    }
-}
-
 impl PartialEq for Action {
-
     fn eq(&self, other: &Self) -> bool {
         self.id == other.id
     }
 }
 
 impl ToString for Action {
-
     pub fn to_string(&self) -> String {
         serde_json::to_string(&self).unwrap()
     }
