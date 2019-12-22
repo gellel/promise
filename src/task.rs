7ab0526837bcc52485e3867e958a9db812d17806
gellel <lindsay.gelle@gmail.com>
2019-12-22 19:19:30 +1100

    Added trait ToString to pub struct Task

--- src/task.rs
+++ src/task.rs
@@ -5,7 +5,7 @@ use contract::{Contract};
 use serde::{Deserialize, Serialize};
 use std::collections::{HashSet};
 
-
+#[derive(Debug, Deserialize, Serialized)]
 pub struct Task {
     pub category: String,
     pub completed: bool,
@@ -15,33 +15,15 @@ pub struct Task {
     pub description: String,
     pub end_at: Option<DateTime<Utc>>,
     pub labels: Option<mut HashSet<String>>,
-    pub rating: u128,
+    pub rating: u8,
     pub subcategory: String,
     pub started: bool,
     pub started_at: Option<DateTime<Utc>>,
     pub user_id: String,
 }
 
-impl Task {
-
-    pub fn end_task(&mut self) -> {
-        self.completed_at = Some(Utc::now())
-        self.completed = true;
-    }
-    
-    pub fn start_task(&mut self) -> bool {
-        self.started_at = Some(Utc::now())
-        self.started = true
-    }
-}
-
-impl Task {
-
-    pub fn add_contract(&mut self, contract: &Contract) -> bool {
-        if contract.signed { self.contracts.insert(contract.id) } else { false }
-    }
-
-    pub fn del_contract(&mut self, contract: &Contract) -> bool {
-        self.contracts.delete(contract.id)
+impl ToString for Task {
+    fn to_string(&self) -> String {
+        serde_json::to_string(&self)
     }
 }
