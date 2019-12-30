00ffe4418dcf19cb26a139ade1d66f1e61282e47
Lindsay Gelle <lindsay.gelle@gmail.com>
2019-12-30 23:27:51 +1100

    Adding methods to pub struct Cx<T>

--- src/cx.rs
+++ src/cx.rs
@@ -5,12 +5,13 @@ use uuid::{Uuid};
 #[derive(Debug, Default, Deserialize, Serialize)]
 pub struct Cx<T> {
     create_at: usize,
-    expire_at: usize,
+    expire_at: Option<usize>,
     from_key: T,
     is_complete: bool,
-    is_ephemeral: bool,
+    is_ephemeral: Option<bool>,
     is_expire: bool,
     is_sign: bool,
+    is_symmetrical: bool,
     owner: Uuid,
     sign_at: usize,
     sign_from: Uuid,
@@ -20,5 +21,16 @@ pub struct Cx<T> {
 }
 
 impl<T> Cx<T> {
-
+}
+impl<T> Cx<T> {
+    pub fn set_sign(&mut self, sign_from: Uuid, sign_to: Uuid) {
+        self.set_sign_from(sign_from);
+        self.set_sign_to(sign_to);
+    }
+    pub fn set_sign_from(&mut self, sign_from: Uuid) {
+        self.sign_from = sign_from;
+    }
+    pub fn set_sign_to(&ut self, sign_to: Uuid) {
+        self.sign_to = sign_to;
+    }
 }
