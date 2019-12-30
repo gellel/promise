9748ba373060eb179c73f806f6dfa8c7df4a357b
Lindsay Gelle <lindsay.gelle@gmail.com>
2019-12-31 00:01:34 +1100

    Update cx.rs

--- src/cx.rs
+++ src/cx.rs
@@ -5,7 +5,7 @@ use uuid::{Uuid};
 #[derive(Debug, Default, Deserialize, Serialize)]
 pub struct Cx<T> {
     create_at: usize,
-    expire_at: Option<usize>,
+    expire_at: usize,
     from_key: T,
     is_complete: bool,
     is_ephemeral: Option<bool>,
@@ -22,9 +22,17 @@ pub struct Cx<T> {
 
 impl<T> Cx<T> {
     pub fn set_expire_at(&mut self, expire_at: usize) {
-        self.expire_at(expire_at);
+        self.expire_at = expire_at;
+        self.is_expire();
     }
 }
+
+impl<T> Cx<T> {
+    pub fn set_is_expire(&mut self) {
+        self.is_expire = !(self.expire_at == 0);
+    }
+}
+
 impl<T> Cx<T> {
     pub fn set_sign(&mut self, sign_from: Uuid, sign_to: Uuid) {
         self.set_sign_from(sign_from);
@@ -33,7 +41,7 @@ impl<T> Cx<T> {
     pub fn set_sign_from(&mut self, sign_from: Uuid) {
         self.sign_from = sign_from;
     }
-    pub fn set_sign_to(&ut self, sign_to: Uuid) {
+    pub fn set_sign_to(&mut self, sign_to: Uuid) {
         self.sign_to = sign_to;
     }
 }
